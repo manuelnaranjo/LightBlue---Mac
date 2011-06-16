01f252d7f5ae7dfa57ee31ab79ee68c38e936c9a
Manuel Francisco Naranjo <manuel@aircable.net>
2011-06-16 12:54:27 -0300

    Make setup.py compatible with virtualenv for Mac switch between release 10.5.0 and 10.6.0 MAC versions

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 # On Python for Series 60, use the SIS files instead.
 
 from distutils.core import setup, Extension
+import platform
 import sys
 
 LINUX = sys.platform.startswith("linux")
 MAC = sys.platform.startswith("darwin")
+if MAC:
+    RELEASE = [int(b) for b in  platform.release().split(".")]
 
 def getpackagedir():
     if MAC:
@@ -30,17 +33,24 @@ def getextensions():
         return [linux_ext, linux_obex_ext]
     return []
 
+packages  = ["lightblue",]
+package_dir = {"lightblue":getpackagedir()}
+
+if MAC and RELEASE >= [10,6,0]:
+    packages.append("LightAquaBlue")
+    package_dir["LightAquaBlue"]="src/LightAquaBlue"
+
 # install the main library
 setup(name="lightblue",
-    version="0.4",
+    version="0.4.1",
     author="Bea Lam",
     author_email="blammit@gmail.com",
     url="http://lightblue.sourceforge.net",
     description="Cross-platform Python Bluetooth library for Mac OS X, GNU/Linux and Python for Series 60.",
     long_description="LightBlue is a cross-platform Python Bluetooth library for Mac OS X, GNU/Linux and Python for Series 60. It provides support for device and service discovery (with and without end-user GUIs), a standard socket interface for RFCOMM sockets, sending and receiving of files over OBEX, advertising of RFCOMM and OBEX services, and access to local device information.",
     license="GPL",
-    packages=["lightblue"],
-    package_dir={"lightblue":getpackagedir()},
+    packages=package,
+    package_dir=package_dir,
     ext_modules=getextensions(),
     classifiers = [ "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
@@ -56,11 +66,29 @@ setup(name="lightblue",
     )
 
 
+def install_LightAquaBlue():
+    import os, sys
+    from os import path
+    dstroot = "/" if not hasattr(sys, 'real_prefix') else \
+       path.dirname(path.dirname(sys.prefix))
+
+    if RELEASE >= [10, 6, 0]:
+        os.chdir("src/mac/LightAquaBlue-10.6.0")
+    else:
+        os.chdir("src/mac/LightAquaBlue-10.5.0")
+    args = ['xcodebuild', 'install', 
+        '-target'. 'LightAquaBlue',
+        '-configuration', 'Release',
+        'DSTROOT=%s' % dstroot,
+        'INSTALL_PATH=/Library/Frameworks',
+        'DEPLOYMENT_LOCATION=YES'
+    print " ".join(args)
+    os.system(" ".join(args))
+
+
 # On Mac, install LightAquaBlue framework
 # if you want to install the framework somewhere other than /Library/Frameworks
 # make sure the path is also changed in LightAquaBlue.py (in src/mac)
 if MAC:
     if "install" in sys.argv:
-        import os
-        os.chdir("src/mac/LightAquaBlue")
-        os.system("xcodebuild install -target LightAquaBlue -configuration Release DSTROOT=/ INSTALL_PATH=/Library/Frameworks DEPLOYMENT_LOCATION=YES")
+        install_LightAquaBlue()
